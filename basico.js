c301d59afee47e40981a8d88dcf44dd13637cd90
Kaian Samuel <kaiansamuelster@gmail.com>
2023-04-27 21:21:48 -0300

    Tipos Numéricos e Booleanos

---
+++ basico.js
@@ -0,0 +1,11 @@
+"use strict";
+//string
+const a = 'Hello World!';
+console.log(a);
+//numbers
+let idade = 41;
+idade = 41.5;
+console.log(`A idade é ${idade}`);
+//boolean
+let possuiHobbies = false;
+console.log(`Tem hobbie? ${possuiHobbies}`);
---
+++ basico.ts
@@ -0,0 +1,12 @@
+//string
+const a: string = 'Hello World!'
+console.log(a)
+
+//numbers
+let idade = 41
+idade = 41.5
+console.log(`A idade é ${idade}`)
+
+//boolean
+let possuiHobbies = false
+console.log(`Tem hobbie? ${possuiHobbies}`)
